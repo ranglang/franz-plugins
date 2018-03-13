e1e884e25935d8998f03dac7986c8cdf2ee4f15f
rang <lanziwen@outlook.com>
2018-03-13 15:56:29 +0800

    chore

--- install.sh
+++ install.sh
@@ -7,14 +7,8 @@ apps=`ls apps`
 
 for line in ${apps}; do 
     echo $line
-	cp -rf franz-recipe-bitbucket/* ${path}/apps/${line}
+	cp -rf franz-recipe-template/* ${path}/apps/${line}
 	./apps/$line/generate.sh
 	mkdir -p ~/.config/Franz/recipes/dev
 	cp -rf ./apps/${line} ~/.config/Franz/recipes/dev
 done
-
-# cp -rf franz-recipe-bitbucket/* apps/xiaomiquan
-# ./apps/xiaomiquan/generate.sh
-
-
-# cp -rf ./apps/xiaomiquan ~/.config/Franz/dev/xiaomiquan
